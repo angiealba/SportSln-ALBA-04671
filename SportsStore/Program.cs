83528a1fdac729da65cd3eb47544248c9d6857a3
Angela Monique Alba <79003193+angiealba@users.noreply.github.com>
2024-05-18 13:55:28 +0800

    Initial commit

---
+++ SportsStore/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(opts => 
+{
+    opts.UseSqlServer(
+    builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+} ) ;
+
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
+
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.UseStaticFiles();
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "catpage",
+    pattern: "{category}/Page{productPage:int}",
+    defaults: new { Controller = "Home", action = "Index" }
+);
+
+app.MapControllerRoute(
+    name: "page",
+    pattern: "Page{productPage:int}",
+    defaults: new { Controller = "Home", action = "Index", productPage = 1 }
+);
+
+app.MapControllerRoute(
+    name: "category",
+    pattern: "{category}",
+    defaults: new { Controller = "Home", action = "Index", productPage = 1 }
+);
+
+app.MapControllerRoute(
+    name: "pagination",
+    pattern: "Products/Page{productPage}",
+    defaults: new { Controller = "Home", action = "Index", productPage = 1 }
+);
+
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
+SeedData.EnsurePopulated(app);
+
+app.Run();
